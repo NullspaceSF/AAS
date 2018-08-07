5a00cd63401973f92a4b6eb9dfcb6dd432eb137f
dfreezy <dreevessf@gmail.com>
2018-08-07 01:22:53 -0700

    added logging

--- Training.py
+++ Training.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import threading
 import numpy as np
 import os
-
+import multiprocessing
 import Datasets
 from Input import Input as Input
 from Input import batchgenerators as batchgen
@@ -12,9 +12,12 @@ import Models.Unet
 import Utils
 import cPickle as pickle
 import Test
+import logging
 
-# import logging
-#
+logger = multiprocessing.log_to_stderr()
+logger.setLevel(logging.DEBUG)
+logger.warning('-----wtf?------')
+
 # logger = logging.getLogger('scope.name')
 #
 # file_log_handler = logging.FileHandler('logfile.log')
@@ -49,7 +52,7 @@ def cfg():
                     "init_disc_lr" : 5e-5, # Discriminator(s) learning rate
                     "init_sup_sep_lr" : 5e-5, # Supervised separator learning rate
                     "init_unsup_sep_lr" : 5e-5, # Unsupervised separator learning rate
-                    "epoch_it" : 2, # Number of supervised separator steps per epoch
+                    "epoch_it" : 1, # Number of supervised separator steps per epoch
                     "num_disc": 5,  # Number of discriminator iterations per separator update
                     "num_frames" : 128, # DESIRED number of time frames in the spectrogram per sample (this can be increased when using U-net due to its limited output sizes)
                     "num_fft" : 512, # FFT Size
@@ -57,12 +60,12 @@ def cfg():
                     'expected_sr' : 16384, # Downsample all audio input to this sampling rate
                     'mono_downmix' : True, # Whether to downsample the audio input
                     'cache_size' : 72, # was 64 Number of audio excerpts that are cached to build batches from !!!64!!
-                    'num_workers' : 6, # was 4 Number of processes reading audio and filling up the cache
+                    'num_workers' : 4, # was 4 Number of processes reading audio and filling up the cache
                     "duration" : 5, # Duration in seconds of the audio excerpts in the cache (excluding input context)
                     'min_replacement_rate' : .2,  # roughly: how many cache entries to replace at least per batch on average. Can be fractional
                     'num_layers' : 4, # How many U-Net layers
                     }
-    experiment_id = np.random.randint(0, 1000000)
+    experiment_id = experiment_id
 
 
 @ex.capture
@@ -405,7 +408,7 @@ def optimise(dataset, supervised):
     model_path = None
     worse_epochs = 0
     best_model_path = ""
-    while worse_epochs < 1:
+    while worse_epochs < 2: #TODO: change back to 1!
         print("EPOCH: " + str(epoch))
         model_path = train(sup_dataset=dataset["train_sup"], unsup_dataset=unsup_dataset, model_folder=model_folder, load_model=model_path)
         curr_loss = test(audio_list=dataset["valid"], model_folder=model_folder, load_model=model_path)
